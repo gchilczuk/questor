cd847b51b278c7cdc6ac5080fa08b089ffb1bff6
Grzegorz Chilczuk <chilczukg@gmail.com>
2018-07-06 22:57:41 +0200

    add basic question structure

--- supervisor/models.py
+++ supervisor/models.py
@@ -1,3 +1,12 @@
+import uuid
+
+from django.contrib.postgres.fields import HStoreField
 from django.db import models
 
-# Create your models here.
+
+class Quest(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    description = models.TextField()
+    question = models.CharField(max_length=100)
+    answer = models.CharField(max_length=24)
+    rejections = HStoreField()
